f35bf7714801f3986bfb3739b5b01736e25359f2
carlosca <carloscarmonaalcantara@gmail.com>
2017-08-10 10:45:06 +0200

    refactor: es6

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# runnerty-executor-filesystem
-Runnerty module: File system executor
+# Filesystem executor for [Runnerty]:
+
+### Configuration sample:
+```json
+{
+  "id":"fs_default",
+  "type":"@runnerty-executor-filesystem"
+}
+```
+
+### Plan sample:
+```json
+{
+ "id":"fs_default",
+ "path": "/etc/runnerty/*.log",
+ "operation": "stat"
+}
+```
+
+```json
+{
+ "id":"fs_default",
+ "path": ["/etc/runnerty/*.log","/etc/runnerty/*.zip"],
+ "operation": "ls",
+ "options":{
+   "orderBy": {"attribute":["size","file"],
+               "order":"desc"}
+ }
+}
+```
+
+### Operation
+* stat: 
+* ls: 
+* mkdir: 
+
+### Output (Process values):
+#### Standard
+* `PROCESS_EXEC_MSG_OUTPUT`: Array files/directories stats. 
+* `PROCESS_EXEC_ERR_OUTPUT`: Error output message.
+#### stat output
+#### ls output
+#### mkdir output
+
+[Runnerty]: http://www.runnerty.io
--- index.js
+++ index.js
@@ -1,294 +1,294 @@
-'use strict';
+"use strict";
 
-const fs = require('fs-extra');
-const glob = require('glob');
-const concat = require('concat-files');
-const path = require('path');
-const bytes = require('bytes');
-const async = require('async');
+const fs = require("fs-extra");
+const bytes = require("bytes");
+const path = require("path");
+const glob = require("glob");
+const lodash = require("lodash");
+const {promisify} = require("util");
+const globAsync = promisify(glob);
 
 var Execution = global.ExecutionClass;
 
-
-class filesystemExecutor extends Execution {
+class fileSystemExecutor extends Execution {
   constructor(process) {
     super(process);
   }
 
   exec(params) {
-
     let _this = this;
-    let operation = params.operation;
-    let inputPath = params.path;
-    let insensitiveCase = params.insensitiveCase;
-    let options = params.options;
-  
+    const operation = params.operation;
+    const inputPath = params.path;
+    const insensitiveCase = params.insensitiveCase || true;
+    const options = params.options || {};
+    let endOptions = {};
+
     /**
      * Avaliable operations
      * - ls
      * - mkdir
      */
     switch (operation) {
-      case 'ls':
-        _curatePath(inputPath, function(err, paths) {
-          if (err) {
-            _endError(err);
-          } else {
-            let resultFiles = [];
-            
-            async.each(paths, function (p, callback) {
-              fs.stat(p, function (err, stats) {
-                if (err) {
-                  callback(err);
-                } else {
-                  if (stats.isFile()) {
-                    resultFiles.push({
-                      file: p.split(path.sep).pop(),
-                      path: p,
-                      mtimeMs: stats.mtimeMs,
-                      atimeMs: stats.atimeMs,
-                      ctimeMs: stats.ctimeMs,
-                      atime: stats.atime,
-                      mtime: stats.mtime,
-                      ctime: stats.ctime,
-                      size: stats.size,
-                      sizeH: bytes(stats.size),
-                      isFile: 1,
-                      isDirectory: 0,
-                      exists: 1
-                    });
-                    callback();
-                  } else if (stats.isDirectory()) {
-                    _ls(p, function (err, files) {
-                      if (err) {
-                        callback(err);
-                      } else {
-                        async.each(files, function(file, callback) {
-                          let filePath = path.join(p, file);
-                      
-                          fs.stat(filePath, function(err, stats) {
-                            if (err) {
-                              callback(err);
-                            } else {
-                              resultFiles.push({
-                                file: file,
-                                path: filePath,
-                                mtimeMs: stats.mtimeMs,
-                                atimeMs: stats.atimeMs,
-                                ctimeMs: stats.ctimeMs,
-                                atime: stats.atime,
-                                mtime: stats.mtime,
-                                ctime: stats.ctime,
-                                size: stats.size,
-                                sizeH: bytes(stats.size),
-                                isFile: 0,
-                                isDirectory: 1,
-                                exists: 1
-                              });
-                              callback();
-                            }
-                          });
-                        }, function done(err) {
-                          callback(err);
-                        })
-                    
-                      }
-                    })
-                  } else {
-                    // TODO symbolic links and so...?
-                    callback();
-                  }
-                }
-              })
-          
-            }, function done(err) {
-              if (err) {
-                _endError(err);
-              } else {
-                _endSuccess(resultFiles);
-              }
-            });
+      case "ls":
+      case "stat":
+        let attributesOrderBy = [];
+        let orderAsc = true;
+
+        if(options.orderBy){
+          if (options.orderBy.attribute.constructor !== Array) {
+            options.orderBy.attribute = [options.orderBy.attribute];
           }
-        });
-        break;
-  
-      case 'mkdir':
-        let paths = [];
-        let createdFolders = [];
-        let notCreatedFolders = [];
-    
-        if (inputPath.constructor !== Array) {
-          paths = [inputPath];
-        } else {
-          paths = inputPath;
-        }
-    
-        async.each(paths, function(path, callback) {
-          fs.mkdirs(path, function (err) {
-            if (err) {
-              notCreatedFolders.push(path);
-              callback();
-            } else {
-              createdFolders.push(path);
-              callback();
+          attributesOrderBy = options.orderBy.attribute;
+          if (options.orderBy.order){
+            if (options.orderBy.order === "desc"){
+              orderAsc = false;
             }
-          });
-        }, function done(err) {
-          if (err) {
-            _endError(err);
-          } else if(notCreatedFolders) {
-            _endError(new Error(`Not created folders: ${notCreatedFolders}`));
-          } else {
-            _endSuccess(true);
           }
-        });
-        break;
-  
-      case 'stat':
-        let pathsStats = [];
-    
-        if (inputPath.constructor !== Array) {
-          inputPath = [inputPath];
         }
-    
-        async.each(inputPath, function(p, callback) {
-          fs.stat(p, function(err, stats) {
-            if (!err) {
-              pathsStats.push({
-                file: p.split(path.sep).pop(),
-                path: p,
-                mtimeMs: stats.mtimeMs,
-                atimeMs: stats.atimeMs,
-                ctimeMs: stats.ctimeMs,
-                atime: stats.atime,
-                mtime: stats.mtime,
-                ctime: stats.ctime,
-                size: stats.size,
-                sizeH: bytes(stats.size),
-                isFile: stats.isFile() ? 1 : 0,
-                isDirectory: stats.isDirectory() ? 1 : 0,
-                exists: 1
-              });
-            } else {
-              pathsStats.push({
-                path: p,
-                exists: 0
-              })
+
+        const isSubDir = (operation === "stat");
+
+        _ls(inputPath, attributesOrderBy, orderAsc, insensitiveCase, isSubDir)
+          .then(res => {
+            endOptions.data_output = res;
+            const firstRow = res[0];
+            if (res.length) {
+              endOptions.extra_output = {
+                first_match_exists: firstRow.exists,
+                first_match_file: firstRow.file,
+                first_match_path: firstRow.path,
+                first_match_mtimeMs: firstRow.mtimeMs,
+                first_match_atimeMs: firstRow.atimeMs,
+                first_match_ctimeMs: firstRow.ctimeMs,
+                first_match_atime: firstRow.atime,
+                first_match_mtime: firstRow.mtime,
+                first_match_ctime: firstRow.ctime,
+                first_match_size: firstRow.size,
+                first_match_sizeH: firstRow.sizeH
+              };
             }
-            callback();
+            _this.end(endOptions);
           })
-        }, function(err) {
-          if (err) {
-            _endError(err);
-          } else {
-            _endSuccess(pathsStats);
-          }
-        });
-    
+          .catch(err => {
+            endOptions.data_output = [];
+            endOptions.extra_output = {
+              first_match_exists: "",
+              first_match_file: "",
+              first_match_path: "",
+              first_match_mtimeMs: "",
+              first_match_atimeMs: "",
+              first_match_ctimeMs: "",
+              first_match_atime: "",
+              first_match_mtime: "",
+              first_match_ctime: "",
+              first_match_size: "",
+              first_match_sizeH: ""
+            };
+            endOptions.end = "error";
+            endOptions.messageLog = err;
+            endOptions.err_output = err;
+            _this.end(endOptions);
+          });
         break;
-        
+      case "mkdir":
+        _mkdirs(inputPath)
+          .then(res =>{
+            let endOptions = {};
+            endOptions.data_output = res;
+            _this.end(endOptions);
+          })
+          .catch(err =>{
+            endOptions.end = "error";
+            endOptions.messageLog = err;
+            endOptions.err_output = err;
+            _this.end(endOptions);
+          });
+        break;
       default:
-        _endError(`Not method found for ${operation}`);
-    }
-
-    function _curatePath(path, callback) {
-      let curatedPaths = [];
-
-      let paths = [];
-
-      if (path.constructor !== Array) {
-        paths = [path];
-      } else {
-        paths = path;
-      }
-
-      async.each(paths, function (p, callback) {
-        glob(p, {nocase: insensitiveCase}, function (err, list) {
-          if (err) {
-            callback(err);
-          } else {
-            let j = list.length;
-            while (j--) {
-              curatedPaths.push(list[j]);
-            }
-            callback();
-          }
-        });
-      }, function done(err) {
-        callback(err, curatedPaths);
-      });
-    }
-
-    function _ls(path, callback) {
-      if (path !== undefined) {
-        fs.readdir(path, function (err, list) {
-          callback(err, list);
-        })
-      } else {
-        callback(new Error('Undefined path'));
-      }
-    }
-
-    function _compareValues(key, order='desc') {
-      return function(a, b) {
-        if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-          return 0;
-        }
-
-        const varA = (typeof a[key] === 'string') ?
-            a[key].toUpperCase() : a[key];
-        const varB = (typeof b[key] === 'string') ?
-            b[key].toUpperCase() : b[key];
-
-        let comparison = 0;
-        if (varA > varB) {
-          comparison = 1;
-        } else if (varA < varB) {
-          comparison = -1;
-        }
-        return (
-            (order === 'desc') ? (comparison * -1) : comparison
-        );
-      };
-    }
-
-    function _endError(err, messageLog) {
-      let endOptions = {};
-      endOptions.end = 'error';
-      endOptions.messageLog = messageLog
-        ? `Dir Error ${operation}: ${err}`
-        : `Dir Error ${operation}: ${messageLog}`;
-      endOptions.err_output = err;
-      _this.end(endOptions);
-    }
-
-    function _endSuccess(res) {
-      // if it's a sortable result operation, will do it if defined in params
-      // (by selected attribute and order direction)
-      if (res && options && options.orderBy) {
-        res.data_output.sort(_compareValues(options.orderBy.attribute, options.orderBy.order));
-      }
-
-      let endOptions = {};
-      endOptions.extra_output = {};
-      endOptions.data_output = res;
-      if (res.length){
-        endOptions.extra_output.first_match_exists  = res[0].exists;
-        endOptions.extra_output.first_match_file  = res[0].file;
-        endOptions.extra_output.first_match_path  = res[0].path;
-        endOptions.extra_output.first_match_mtimeMs  = res[0].mtimeMs;
-        endOptions.extra_output.first_match_atimeMs  = res[0].atimeMs;
-        endOptions.extra_output.first_match_ctimeMs  = res[0].ctimeMs;
-        endOptions.extra_output.first_match_atime  = res[0].atime;
-        endOptions.extra_output.first_match_mtime  = res[0].mtime;
-        endOptions.extra_output.first_match_ctime  = res[0].ctime;
-        endOptions.extra_output.first_match_size  = res[0].size;
-        endOptions.extra_output.first_match_sizeH  = res[0].sizeH;
-      }
-
-      _this.end(endOptions);
+        endOptions.end = "error";
+        endOptions.messageLog = `Not method found for ${operation}`;
+        endOptions.err_output = `Not method found for ${operation}`;
+        _this.end(endOptions);
     }
   }
 }
 
-module.exports = filesystemExecutor;
+/**
+ * Return array with all files and directories founds by paths indicates
+ * @param paths (glob compatible)
+ * @param orders:  array of field ordering
+ * @param orderAsc: boolean asc (true) or desc order
+ * @param insensitiveCase: insensitive case glob
+ * @param isSubDir: if set true and paths is a folder function return stat info.
+ *                  if set false return recurrent contain folder stats.
+ * @returns {Promise} - array:  stats results files and folders.
+ * @private
+ */
+function _ls(paths, orders = [], orderAsc = true, insensitiveCase = true, isSubDir = true) {
+  return new Promise((resolve,reject) => {
+    if (paths.constructor !== Array) {
+      paths = [paths];
+    }
+
+    _curatePath(paths, insensitiveCase)
+      .then(curatePaths =>{
+        let pathsLsPromises = [];
+        curatePaths.map(_path => {
+          // If _ls paths input is the same that _curatePath output is not a curate path, return content.
+          if (paths.indexOf(_path) !== -1) isSubDir = false;
+          pathsLsPromises.push(lsAsync(_path, isSubDir));
+        });
+
+        Promise.all(pathsLsPromises)
+          .then(values => {
+            let res = [].concat(...values);
+            if (orders.length) res = lodash.sortBy(res, orders);
+            if (!orderAsc) res = res.reverse();
+            resolve(res);
+          });
+      })
+      .catch(err =>{
+        reject(err);
+      });
+  });
+}
+
+/**
+ * Return array with all dirs created or reject with error.
+ * If dir exists return null into te array.
+ * @param path
+ * @returns {Promise} - array
+ * @private
+ */
+function _mkdirs(paths) {
+  return new Promise((resolve,reject) => {
+    if (paths.constructor !== Array) {
+      paths = [paths];
+    }
+
+    let pathsEnsureDirPromises = [];
+    paths.map(_path => {
+      pathsEnsureDirPromises.push(fs.ensureDir(_path));
+    });
+
+    Promise.all(pathsEnsureDirPromises)
+      .then(values => {
+        let res = [].concat(...values);
+        resolve(res);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
+/**
+ * Return array with all coincident glob result from path or paths input
+ * @param path
+ * @param insensitiveCase
+ * @returns {Promise} - array
+ * @private
+ */
+function _curatePath(paths, insensitiveCase = true) {
+  return new Promise((resolve,reject) => {
+    if (paths.constructor !== Array) {
+      paths = [paths];
+    }
+
+    let pathsGlobPromises = [];
+    paths.map(_path => {
+      pathsGlobPromises.push(globAsync(_path, {nocase: insensitiveCase}));
+    });
+
+    Promise.all(pathsGlobPromises)
+      .then(values => {
+        const res = [].concat(...values);
+        if (res.length){
+          resolve(res);
+        }else{
+          reject("No results found for indicated paths");
+        }
+      });
+  });
+}
+
+/**
+ * Return folder content
+ * @param path
+ * @returns {Promise}
+ * @private
+ */
+function _readdirAsync(path) {
+  return new Promise(function (resolve, reject) {
+    if (path !== undefined) {
+      fs.readdir(path, function (err, list) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(list);
+        }
+      });
+    } else {
+      reject("Undefined path");
+    }
+  });
+}
+
+/**
+ * Return async stats files and folders
+ * @param file
+ * @param isSubDir
+ * @returns {Promise}
+ */
+function lsAsync(file, isSubDir = false) {
+  return new Promise(function (resolve, reject) {
+    fs.stat(file, (err, stat) => {
+      if (err) {
+        reject(err);
+      } else {
+        let res;
+        if (stat.isFile() || (stat.isDirectory() && isSubDir)) {
+          res = {
+            file: file.split(path.sep).pop(),
+            path: file,
+            mtimeMs: stat.mtimeMs,
+            atimeMs: stat.atimeMs,
+            ctimeMs: stat.ctimeMs,
+            atime: stat.atime,
+            mtime: stat.mtime,
+            ctime: stat.ctime,
+            size: stat.size,
+            sizeH: bytes(stat.size),
+            isFile: stat.isFile()?1:0,
+            isDirectory: stat.isDirectory()?1:0,
+            exists: 1
+          };
+          resolve(res);
+        } else {
+          if (stat.isDirectory()) {
+            _readdirAsync(file)
+              .then(files =>{
+                let resPromises = [];
+                files.map(f => {
+                  resPromises.push(lsAsync(path.join(file, f), true));
+                });
+
+                Promise.all(resPromises)
+                  .then(res => {
+                    res = res.filter(Boolean);
+                    resolve(res);
+                  });
+              })
+              .catch(err => {
+                reject(err);
+              });
+          } else {
+            reject(`File is not valid: ${file}`);
+          }
+        }
+      }
+    });
+  });
+}
+
+module.exports = fileSystemExecutor;
